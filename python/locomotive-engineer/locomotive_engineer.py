b1640f219cd4c816c85c231b4f081cd56e0b016e
Vincent Ging Ho Yim <cenviity@gmail.com>
2023-08-18 16:27:02 +1000

    Solve exercise

--- python/locomotive-engineer/locomotive_engineer.py
+++ python/locomotive-engineer/locomotive_engineer.py
@@ -1,13 +1,13 @@
 """Functions which helps the locomotive engineer to keep track of the train."""
 
 
-def get_list_of_wagons():
+def get_list_of_wagons(*wagons):
     """Return a list of wagons.
 
     :param: arbitrary number of wagons.
     :return: list - list of wagons.
     """
-    pass
+    return list(wagons)
 
 
 def fix_list_of_wagons(each_wagons_id, missing_wagons):
@@ -17,17 +17,21 @@ def fix_list_of_wagons(each_wagons_id, missing_wagons):
     :parm missing_wagons: list - the list of missing wagons.
     :return: list - list of wagons.
     """
-    pass
+    first, second, third, *rest = each_wagons_id
+
+    return [third, *missing_wagons, *rest, first, second]
 
 
-def add_missing_stops():
+def add_missing_stops(route, **stops):
     """Add missing stops to route dict.
 
     :param route: dict - the dict of routing information.
     :param: arbitrary number of stops.
     :return: dict - updated route dictionary.
     """
-    pass
+    all_stops = list(stops.values())
+
+    return {**route, "stops": all_stops}
 
 
 def extend_route_information(route, more_route_information):
@@ -37,7 +41,7 @@ def extend_route_information(route, more_route_information):
     :param more_route_information: dict -  extra route information.
     :return: dict - extended route information.
     """
-    pass
+    return {**route, **more_route_information}
 
 
 def fix_wagon_depot(wagons_rows):
@@ -46,4 +50,14 @@ def fix_wagon_depot(wagons_rows):
     :param wagons_rows: list[list[tuple]] - the list of rows of wagons.
     :return: list[list[tuple]] - list of rows of wagons.
     """
-    pass
+    col1 = []
+    col2 = []
+    col3 = []
+
+    for row in wagons_rows:
+        wagon1, wagon2, wagon3 = row
+        col1.append(wagon1)
+        col2.append(wagon2)
+        col3.append(wagon3)
+
+    return [col1, col2, col3]
