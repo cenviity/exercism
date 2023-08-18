12513b04db41998657669d10175bfe381f20020f
Vincent Ging Ho Yim <cenviity@gmail.com>
2023-08-18 16:44:36 +1000

    Format imports

--- python/cater-waiter/sets.py
+++ python/cater-waiter/sets.py
@@ -1,13 +1,15 @@
 """Functions for compiling dishes and ingredients for a catering company."""
 
 
-from sets_categories_data import (VEGAN,
-                                  VEGETARIAN,
-                                  KETO,
-                                  PALEO,
-                                  OMNIVORE,
-                                  ALCOHOLS,
-                                  SPECIAL_INGREDIENTS)
+from sets_categories_data import (
+    VEGAN,
+    VEGETARIAN,
+    KETO,
+    PALEO,
+    OMNIVORE,
+    ALCOHOLS,
+    SPECIAL_INGREDIENTS,
+)
 
 
 def clean_ingredients(dish_name, dish_ingredients):
