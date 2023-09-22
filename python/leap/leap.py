3937cac005c0a3f02ab9cff875889b04ee666e56
Vincent Ging Ho Yim <cenviity@gmail.com>
2023-09-22 17:05:12 +1000

    Solve exercise

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,2 +1,8 @@
 def leap_year(year):
-    pass
+    if year % 4 == 0:
+        if year % 100 == 0:
+            return year % 400 == 0
+        else:
+            return True
+    else:
+        return False
