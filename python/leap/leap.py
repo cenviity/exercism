4a79b4ac4a6b6461d7582a93e566214500572bb8
Vincent Ging Ho Yim <cenviity@gmail.com>
2023-09-25 10:16:47 +1000

    Add module and function docstrings

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,4 +1,16 @@
+"""This module contains a single function that tests if a given year is a leap year.
+"""
+
+
 def leap_year(year):
+    """Determine whether a given year is a leap year.
+
+    Args:
+        year (int): A given year to be tested.
+
+    Returns:
+        bool: True if `year` is a leap year, otherwise False.
+    """
     if year % 400 == 0:
         return True
 
